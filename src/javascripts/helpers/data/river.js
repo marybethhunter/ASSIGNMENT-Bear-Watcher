0021f4a6248cbae2f56a8e96587a191d0a8df2c7
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2021-08-23 16:40:25 -0500

    added readme

--- src/javascripts/helpers/data/river.js
+++ src/javascripts/helpers/data/river.js
@@ -10,14 +10,6 @@ const buildBearCard = (array) => {
       <img src="${bear.image}" class="card-img-top" alt="${bear.name}">
       <div class="card-body">
         <p class="card-text">${bear.name}</p>
-        <div class="bearFishBtns">
-          <button id="fishAttempt">Tried to Catch Fish: </button>
-          <button id="fishSuccess">Successfully Caught Fish: </button>
-          <p id="fishCounter">Number of fish caught: </p>
-        </div>
-        <div>
-        <p>Timestamp:</p>
-        </div>
       </div>
    </div>
   `;
@@ -36,22 +28,4 @@ const handleFormSubmit = (event) => {
   document.querySelector('form').reset();
 };
 
-const fishAttemptButton = document.getElementById('#fishAttempt');
-let counter = 0;
-
-const incrementFishAttempt = () => {
-  fishAttemptButton.innerHTML = counter.toString();
-  counter += 1;
-  return counter;
-};
-
-// const handleFishAttempt = (event) => {
-//   let count = 0;
-//   const attemptBtn = document.querySelector('#fishAttempt');
-//   if (event.target.id === attemptBtn) {
-//     count += 1;
-//     attemptBtn.innerHTML = `Tried to Catch Fish: ${count}`;
-//   }
-// };
-
-export { handleFormSubmit, incrementFishAttempt };
+export default handleFormSubmit;
